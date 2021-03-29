00aa6c9f3bf7d01b26bc8472decfbcfdc64b26ea
r-goto <https://gl.creationline.com/r-goto>
2021-03-29 17:18:28 +0900

    Added .chef directory

---
+++ .chef/config.rb
@@ -0,0 +1,13 @@
+current_dir = File.dirname(__FILE__)
+  user = 'r-goto'
+  node_name                user
+  client_key               "#{current_dir}/r-goto.pem"
+  chef_server_url          "https://automate.cl/organizations/first-org"
+  cookbook_path            ["#{current_dir}/../cookbooks"]
+  cookbook_copyright       "First Org Cookbooks"
+  cookbook_license         "Apache-2.0"
+  cookbook_email           "cookbooks@first.org"
+
+  # Amazon AWS
+  knife[:aws_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+  knife[:aws_secret_access_key] = ENV['AWS_SECRET_ACCESS_KEY']
