fd00db463b7e8fed5035a50904f49810cb0f3b1a
Ryuji Goto <g.ryuji0510@gmail.com>
2021-04-02 19:52:13 +0900

    Removed unnecessary `chef-client` cookbook

--- policyfiles/db-server.rb
+++ policyfiles/db-server.rb
@@ -7,8 +7,7 @@ name 'db-server'
 default_source :chef_server, 'https://automate.cl/organizations/first-org'
 run_list 'bootstrap_a_node::default', 'postgresql_setup::default'
 cookbook 'bootstrap_a_node', '2.3.2'
-cookbook 'postgresql_setup'
-cookbook 'chef-client', '~> 12.3.3'
+cookbook 'postgresql_setup', '0.1.0'
 
 ##########
 # Attributes
@@ -25,8 +24,8 @@ override['bootstrap_a_node']['chef_client']['version'] = '16'
 override['chef_client']['handler']['slack']['enabled'] = false
 
 # Specify CCR interval and splay
-override['chef_client']['interval'] = 43200 # 12H
-override['chef_client']['splay'] = 21600 # 6H
+override['chef_client']['interval'] = 86400 # 12H
+override['chef_client']['splay'] = 3600 # 6H
 
 # Specify node Time Zone
 override['bootstrap_a_node']['timezone'] = 'Asia/Tokyo'
