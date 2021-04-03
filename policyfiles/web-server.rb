a19f9f29c48354dd19e278d209f67ee4378da874
Ryuji Goto <g.ryuji0510@gmail.com>
2021-04-03 10:53:25 +0900

    Update web-server.rb

--- policyfiles/web-server.rb
+++ policyfiles/web-server.rb
@@ -6,9 +6,8 @@
 name 'web-server'
 default_source :chef_server, 'https://automate.cl/organizations/first-org'
 run_list 'bootstrap_a_node::default', 'nginx_setup::default'
-cookbook 'bootstrap_a_node', '2.3.2'
-cookbook 'nginx_setup', '1.0.0' # , git: 'git@github.com:first-org-chef-repo/nginx_setup-cookbook.git', tag: 'v1.1'
-# cookbook 'nginx_setup', '2.0.0', git: 'git@github.com:first-org-chef-repo/nginx_setup-cookbook.git', tag: 'v2.0'
+cookbook 'bootstrap_a_node', '3.0.0'
+cookbook 'nginx_setup', '1.0.0'
 
 ##########
 # Attributes
@@ -23,7 +22,7 @@ override['bootstrap_a_node']['policy_name'] = 'web-server'
 override['bootstrap_a_node']['policy_group'] = 'staging'
 
 # Specify chef-client version
-override['bootstrap_a_node']['chef_client']['version'] = '16'
+override['bootstrap_a_node']['chef_client']['version'] = '16.10'
 
 # Disable Slack Notification that's set only for the initial CCR
 override['chef_client']['handler']['slack']['enabled'] = false
