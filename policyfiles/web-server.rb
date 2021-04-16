1396bb6e70b4f871ca92d19434e0106221247912
r-goto <https://gl.creationline.com/r-goto>
2021-04-16 11:42:36 +0900

    Changed CCR interval to 30mins

--- policyfiles/web-server.rb
+++ policyfiles/web-server.rb
@@ -15,8 +15,8 @@ cookbook 'ssh_setup', '1.0.0'
 ##########
 
 # Specify CCR interval and splay
-override['chef_client']['interval'] = 86400 # 24H
-override['chef_client']['splay'] = 0
+override['chef_client']['interval'] = 1800
+override['chef_client']['splay'] = 300
 
 # Specify Policy name & Policy group
 override['bootstrap_a_node']['policy_name'] = 'web-server'
