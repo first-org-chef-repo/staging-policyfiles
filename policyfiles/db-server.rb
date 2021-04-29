5e4487fd6b155ac338d8a69a5caa93ae8f11804f
r-goto <https://github.com/r-goto>
2021-04-29 22:22:34 +0900

    Changed bootstrap_a_node to node_setup

--- policyfiles/db-server.rb
+++ policyfiles/db-server.rb
@@ -5,8 +5,8 @@
 
 name 'db-server'
 default_source :chef_server, 'https://chef-automate.creationline.com/organizations/first-org'
-run_list 'bootstrap_a_node::default', 'postgresql_setup::default', 'ssh_setup::default'
-cookbook 'bootstrap_a_node', '1.0.0'
+run_list 'node_setup::default', 'postgresql_setup::default', 'ssh_setup::default'
+cookbook 'node_setup', '1.0.0'
 cookbook 'postgresql_setup', '1.0.0'
 cookbook 'ssh_setup', '2.0.0'
 
@@ -19,14 +19,14 @@ override['chef_client']['interval'] = 1800
 override['chef_client']['splay'] = 60
 
 # Specify Policy name & Policy group
-override['bootstrap_a_node']['policy_name'] = 'db-server'
-override['bootstrap_a_node']['policy_group'] = 'staging'
+override['node_setup']['policy_name'] = 'db-server'
+override['node_setup']['policy_group'] = 'staging'
 
 # Specify chef-client version
-override['bootstrap_a_node']['chef_client']['version'] = '17'
-override['bootstrap_a_node']['chef_client']['channel'] = 'stable'
+override['node_setup']['chef_client']['version'] = '17'
+override['node_setup']['chef_client']['channel'] = 'stable'
 
 # Disable Slack Notification that's set only for the initial CCR
 override['chef_client']['handler']['slack']['enabled'] = false
 # Specify node Time Zone
-override['bootstrap_a_node']['timezone'] = 'Asia/Tokyo'
+override['node_setup']['timezone'] = 'Asia/Tokyo'
